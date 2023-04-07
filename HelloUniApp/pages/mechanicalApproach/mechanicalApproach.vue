9ef691cd3dd482db8e2f5ba99a1227f37cda1d6d
jinyanze <17614190247@163.com>
2023-04-07 11:42:03 +0800

    111

--- HelloUniApp/pages/mechanicalApproach/mechanicalApproach.vue
+++ HelloUniApp/pages/mechanicalApproach/mechanicalApproach.vue
@@ -1,19 +1,105 @@
 <template>
-	<view>
-		
+	<view class="mechanicalApproach">
+		<mescroll-body ref="mescrollRef" @init="initData" :fixed="true" :isLock="true" @down="downCallback"
+			@up="upCallback" :optUp="optUp">
+			<view class="item" v-for="item in mechanicalApproachList" @click="toDetail(item)">
+				<view class="header">
+					<view>单据编号: {{item.num}}</view>
+					<view>{{item.dateTime}}</view>
+				</view>
+				<view class="body">
+					<view class="label">制单人: {{item.person}}</view>
+					<view class="label">机械代码: {{item.mechanicalCode}}</view>
+					<view class="label">租赁方式: {{item.leaseLabel}}</view>
+					<view class="label">进场时间: {{item.approachTime}}</view>
+				</view>
+			</view>
+		</mescroll-body>
 	</view>
 </template>
 
 <script>
+	// 懒加载列表
+	import MescrollMixin from "@/uni_modules/mescroll-uni/components/mescroll-uni/mescroll-mixins.js";
 	export default {
+		mixins: [MescrollMixin],
 		data() {
 			return {
-				
+				totalPage: 1,
+				optUp: {
+					offset: '200'
+				},
+				mechanicalApproachList: [],
+				itemObj: {
+					num: 'JC00100004',
+					dateTime: '2023.02.11  10:00:00',
+					person: '李白',
+					mechanicalCode: 'JX00001',
+					leaseLabel: '月租',
+					leaseStatus: '1',
+					approachTime: '2023.03.01'
+				}
 			};
+		},
+		methods: {
+			toDetail(item) {
+				uni.navigateTo({
+					url: '/pages/mechanicalApproachAdd/mechanicalApproachAdd?item=' + JSON.stringify(item)
+				});
+			},
+			initData(mescroll) {
+				this.mescroll = mescroll;
+				this.loadmore()
+			},
+			downCallback() {
+				this.mechanicalApproachList = [];
+				this.totalPage = 1;
+				this.loadmore();
+				setTimeout(() => {
+					this.mescroll.endByPage()
+				}, 800)
+			},
+			upCallback() {
+				this.totalPage += 1;
+				this.loadmore();
+				setTimeout(() => {
+					this.mescroll.endByPage(15)
+				}, 800)
+			},
+			loadmore() {
+				for (let i = 0; i < 15; i++) {
+					this.mechanicalApproachList.push(this.itemObj)
+				}
+			},
 		}
 	}
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
+	.mechanicalApproach {
+		.item {
+			border: 1rpx solid black;
+			border-bottom: none;
 
+			&:last-child {
+				border-bottom: 1rpx solid black;
+			}
+
+			.header {
+				line-height: 80rpx;
+				padding: 0 40rpx;
+				display: flex;
+				justify-content: space-between;
+				border-bottom: 1rpx solid gray;
+			}
+
+			.body {
+				padding: 0 40rpx;
+
+				.label {
+					padding: 10rpx 0;
+				}
+			}
+		}
+	}
 </style>
---
+++ HelloUniApp/pages/mechanicalApproachAdd/mechanicalApproachAdd.vue
@@ -0,0 +1,130 @@
+<template>
+	<view class="mechanicalApproachAdd">
+		<u--form class="yj-form" labelPosition="left" :model="mechanicalApproach" ref="uForm" labelWidth="200rpx">
+			<u-form-item label="机械代码" ref="item1">
+				<u--input class="ta-right" v-model="mechanicalApproach.num" border="none"></u--input>
+			</u-form-item>
+			<u-form-item label="物料编码" ref="item1">
+				<u--input class="ta-right" v-model="mechanicalApproach.mechanicalCode" border="none"></u--input>
+			</u-form-item>
+			<u-form-item label="名称" ref="item1">
+				<u--input class="ta-right" v-model="mechanicalApproach.mechanicalName" border="none"></u--input>
+			</u-form-item>
+			<u-form-item label="规格型号" ref="item1">
+				<u--input class="ta-right" v-model="mechanicalApproach.mechanicalSize" border="none"></u--input>
+			</u-form-item>
+			<u-form-item label="单位" ref="item1">
+				<u--input class="ta-right" v-model="mechanicalApproach.unit" border="none"></u--input>
+			</u-form-item>
+			<u-form-item label="智能设备ID" ref="item1">
+				<u--input class="ta-right" v-model="mechanicalApproach.equipmentId" border="none"></u--input>
+			</u-form-item>
+			<u-form-item label="授权人" ref="item1">
+				<u--input class="ta-right" v-model="mechanicalApproach.person" border="none"></u--input>
+			</u-form-item>
+			<u-form-item label="租赁方式" ref="item1">
+				<uni-data-select class="yj-select" v-model="mechanicalApproach.leaseStatus" :localdata="range"
+					@change="changeLease"></uni-data-select>
+			</u-form-item>
+			<u-form-item label="进场时间" ref="item1">
+				<u--input class="ta-right" v-model="mechanicalApproach.person" border="none"></u--input>
+			</u-form-item>
+			<u-form-item label="备注" ref="item1" labelPosition="top">
+				<u--textarea style="margin-top: 20rpx;" v-model="mechanicalApproach.remark" maxlength="200" count
+					placeholder="请输入内容" border="none"></u--textarea>
+			</u-form-item>
+			<u-divider text="" :hairline="false"></u-divider>
+			<view class="">
+				其它情况
+			</view>
+			<u-form-item label="油料验收" ref="item1" labelPosition="top">
+				<u--textarea style="margin-top: 20rpx;" v-model="mechanicalApproach.remark2" maxlength="100" count
+					placeholder="请输入内容" border="none"></u--textarea>
+			</u-form-item>
+			<u-form-item label="验收图片" ref="item1" labelPosition="top">
+				<u-upload :fileList="fileList" @afterRead="afterRead" @delete="deletePic" name="1" multiple
+					:maxCount="2"></u-upload>
+			</u-form-item>
+		</u--form>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				mechanicalApproach: {
+					num: 'JC00100004',
+					dateTime: '2023.02.11  10:00:00',
+					person: '李白',
+					mechanicalCode: 'JX00001',
+					mechanicalName: '挖掘机',
+					mechanicalSize: 'size1',
+					leaseLabel: '月租',
+					leaseStatus: 1,
+					approachTime: '2023.03.01',
+					unit: 'm³',
+					equipmentId: '1111112',
+					remark: '太丑了,不要',
+					remark2: '95 加满',
+					imageList: []
+				},
+				range: [{
+						value: 1,
+						text: "月租"
+					},
+					{
+						value: 2,
+						text: "月租双班"
+					},
+				],
+				fileList: [],
+			}
+		},
+		onLoad(query) {
+			// if (query.item) {
+			// 	this.mechanicalApproach = JSON.parse(query.item)
+			// }
+		},
+		methods: {
+			changeLease() {
+
+			},
+			// 删除图片
+			deletePic(event) {
+				this.fileList.splice(event.index, 1)
+			},
+			// 新增图片
+			async afterRead(event) {
+				event.file.map(item => {
+					this.fileList.push(item)
+				})
+			},
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.mechanicalApproachAdd {
+		.yj-form {
+			width: 90%;
+			margin: 0 5%;
+
+			.yj-select {
+				/deep/.uni-select {
+					width: 250rpx;
+					margin-left: 220rpx;
+				}
+			}
+
+			.u-divider {
+				width: 120%;
+				margin-left: -10%;
+			}
+		}
+
+		/deep/.uni-input-input {
+			text-align: right;
+		}
+	}
+</style>
